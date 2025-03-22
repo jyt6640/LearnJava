c21b4a938d8089c516c6917a8ad32b69c1b6255a
jyt6640 <jyt6640@gmail.com>
2025-03-22 18:08:56 +0900

    pusg

--- src/ch03/sec01/ArithmeticOperatorExample.java
+++ src/ch03/sec01/ArithmeticOperatorExample.java
@@ -12,5 +12,11 @@ public class ArithmeticOperatorExample {
 
         long result2 = v1 + v2 - v4; //모든 피연산자는 long 타입으로 자동 변환 후 연산
         System.out.println("result2: " + result2);
+
+        double result3 = (double) v1 / v2; //double 타입으로 강제 변환 후 연산
+        System.out.println("result3: " + result3);
+
+        int result4 = v1 % v2;
+        System.out.println("result4: " + result4);
     }
 }
