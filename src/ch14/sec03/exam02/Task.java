683060127f629bece0de8f979f887c0a539a5e2a
jyt6640 <jyt6640@gmail.com>
2025-08-07 17:55:56 +0900

    8월 7일 공부 완료

---
+++ src/ch14/sec03/exam02/Task.java
@@ -0,0 +1,11 @@
+package ch14.sec03.exam02;
+
+public class Task implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try { Thread.sleep(500); } catch (Exception e) {}
+        }
+    }
+}
