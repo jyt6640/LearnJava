4d98c1ae9f5f7a3933b5708942ebaa3a6285d506
jyt6640 <jyt6640@gmail.com>
2025-08-28 20:02:15 +0900

    8월 28일 공부 완(18장 끝)

---
+++ src/ch18/sec10/ObjectOutputStreamExample.java
@@ -0,0 +1,25 @@
+package ch18.sec10;
+
+import java.io.*;
+import java.util.*;
+
+public class ObjectOutputStreamExample {
+    public static void main(String[] args) throws Exception {
+        //FileOutputSTream에 ObjectOutputStream 보조 스트림 연결
+        FileOutputStream fos = new FileOutputStream("C:/Temp/object.dat");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        //객체 생성
+        Member m1 = new Member("fall","단풍이");
+        Product p1 = new Product("노트북", 1500000);
+        int[] arr1 = { 1, 2, 3 };
+
+        //객체를 역직렬화해서 파일에 저장
+        oos.writeObject(m1);
+        oos.writeObject(p1);
+        oos.writeObject(arr1);
+
+        oos.flush(); oos.close(); fos.close();
+    }
+
+}
